0196541a72aee235143274914c0d9e7b9b9dbd78
hubnoodle <deepika.koripelli@stu.jefferson.kyschools.us>
2022-04-07 17:37:10 -0400

    I changed the background color to purple

--- config.js
+++ config.js
@@ -1,9 +1,14 @@
 import firebase from 'firebase';
 
-// add SDK Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+    apiKey: "AIzaSyAKSaXFAEJVU6y6O0Q8vj6FNPR34Rdd6E8",
+    authDomain: "team-voting-705ef.firebaseapp.com",
+    projectId: "team-voting-705ef",
+    storageBucket: "team-voting-705ef.appspot.com",
+    messagingSenderId: "766027629892",
+    appId: "1:766027629892:web:daec743f5c80ab9ebd4311"
+  };
 
-};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
